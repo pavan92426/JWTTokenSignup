0dece4301ced67047de694850753b14afd7c7416
Pavan Kumar Theegala <pavankumar.theegala@gmail.com>
2024-05-22 08:36:57 +0200

    First commit

---
+++ src/main/java/com/proj/willow/WillowfixApplication.java
@@ -0,0 +1,13 @@
+package com.proj.willow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillowfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillowfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proj/willow/WillowfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.willow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillowfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
