cc9317df03a087799e8e5e13df2d023d74cc44b9
Pavan Kumar Theegala <pavankumar.theegala@gmail.com>
2024-06-05 19:34:59 +0200

    First commit

---
+++ src/main/java/com/proj/willow/entity/Role.java
@@ -0,0 +1,6 @@
+package com.proj.willow.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/proj/willow/secure/JWTValidatorAuthFilter.java
@@ -0,0 +1,69 @@
+package com.proj.willow.secure;
+
+import com.proj.willow.service.JwtService;
+import com.proj.willow.service.WillowUserService;
+import com.proj.willow.shared.utils.exceptions.UserServiceException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class JWTValidatorAuthFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    public JWTValidatorAuthFilter(UserDetailsService userDetailsService, JwtService jwtService,
+                                  HandlerExceptionResolver handlerExceptionResolver) {
+        this.userDetailsService = userDetailsService;
+        this.jwtService = jwtService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        try {
+            jwt = authHeader.substring(7);
+            userEmail = jwtService.extractUserName(jwt);
+            if (StringUtils.isNotEmpty(userEmail)
+                    && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    SecurityContext context = SecurityContextHolder.createEmptyContext();
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    context.setAuthentication(authToken);
+                    SecurityContextHolder.setContext(context);
+                }
+            }
+            filterChain.doFilter(request, response);
+
+        } catch (Exception exp) {
+            handlerExceptionResolver.resolveException(request, response, null, exp);
+        }
+
+    }
+}
